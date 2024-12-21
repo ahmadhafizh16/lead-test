e08f973e80d88d1eed52d08de88621723d164015
ahmad h <futsunushi16@gmail.com>
2024-12-21 07:18:51 +0700

    feat: new class CreateUserRequest

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'unique:users',
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+            ],
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'max:50',
+            ],
+        ];
+    }
+}
