ebe598da695c7ec9958a59f27f836585c5f7120c
ahmad h <futsunushi16@gmail.com>
2024-12-22 05:38:13 +0700

    feat: implement new route GET `api/users`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Actions\CreateUserAction;
+use App\Actions\GetAllUserAction;
 use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\GetAllUserRequest;
+use App\Transformers\PaginatedUserTransformer;
 use App\Transformers\UserTransformer;
 use Illuminate\Http\JsonResponse;
 
@@ -11,6 +14,7 @@ class UserController extends Controller
 {
     public function __construct(
         private CreateUserAction $createUserAction,
+        private GetAllUserAction $getAllUserAction,
     ) {
     }
 
@@ -22,5 +26,13 @@ class UserController extends Controller
         // Return a success response with a 201 status code
         // The response should contain the newly created user's data
         return $this->transform($user, new UserTransformer(), 201);
+    }
+
+    public function getAllUser(GetAllUserRequest $request): mixed
+    {
+        $paginateUsers = $this->getAllUserAction->run($request);
+
+        // Return a paginated response with a 200 status code
+        return $this->transform($paginateUsers, new PaginatedUserTransformer(), 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/users', [UserController::class, 'createUser']);
+
+Route::get('/users', [UserController::class, 'getAllUser']);
