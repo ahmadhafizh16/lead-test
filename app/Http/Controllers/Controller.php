6547b4f687a69cdb3a8b7bff8640d3bfa98caa66
ahmad h <futsunushi16@gmail.com>
2024-12-21 07:15:27 +0700

    feat: add transform method to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Transformers\ParentTransformer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
+
 abstract class Controller
 {
-    //
+    protected function transform(mixed $data, ParentTransformer $transformer, int $code = 200): JsonResponse
+    {
+        if (!($transformer instanceof ParentTransformer)) {
+            throw new \Exception('Invalid transformer class', 500);
+        }
+
+        $response = [];
+      
+        if (is_iterable($data)) {
+            $response = Arr::map($data, function ($value) use ($transformer) {
+                return $transformer->transform($value);
+            });
+        } else {
+            $response = $transformer->transform($data);
+        }
+        
+        return response()->json($response, $code);
+    }
 }
